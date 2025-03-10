3ca685f20ef0473d2bd60ff3c9bd897d9be26c65
igor94grozdanic <igor94grozdanic@gmail.com>
2025-03-10 23:37:33 +0100

    New columns added in db and implemented on app

--- Components/list-of-objects.php
+++ Components/list-of-objects.php
@@ -9,11 +9,18 @@
         {
             echo "<div id=".$row["div_id"]." class="."col-lg-4"." data-position=".$row["name"].">".
                     "<div class="."property-entry".">".
-                        "<a href="."property-details.php?object=".$row["order_number"]." class="."property-thumbnail".">".
+                        "<a href=".$row["has_details_path"]." class=".$row["a_active"]." >".
                             "<img src=".$row["image_path"]." alt="."Image"." class="."img-fluid"." style="."width:100%; height:50%".">".
                         "</a>".
                         "<div class="."property-body".">".
-                           "<h2 class="."property-title"."><a href="."property-details.php?object=".$row["order_number"].">".$row["name"]."</a></h2>".
+                           "<div style="."display:inline-block;".">".
+                               "<h2 class=".$row["title_active"].">
+                                   <a href=".$row["has_details_path"].">".$row["name"]."</a>
+                                </h2>".
+                           "</div>".
+                           "<div style="."display:inline-block;margin-left:10px;".">".
+                               "<img src="."images/icons/Star.png"." style="."width:20px;display:".$row["star_visibility"].";"."></img>".
+                           "</div>".
                            "<br><span class="."property-location"."><span class="."icon-room"."></span>".$row["address_city"]."</span>".
                            "<br><br><strong class="."d-block"." style="."color:green; font-size:15px;".">".$row["description"]."</strong>".
                            /*"<br><ul class="."property-specs-wrap".">".
@@ -24,6 +31,7 @@
                                "</ul>".*/
                                "<br/>".
                                "<p id=".$row["project_list_id"]."><b>Kategorija: </b>".$row["category_name"]."</p>".
+                               "<a href=".$row["has_details_path"]." class=".$row["a_active"]." style="."display:".$row["display_details"].">Više detalja</a>".
                                "<p id=".$row["krizan_project_list_id"]." style="."display:none"."><b>".$row["list_of_krizan_products"]."</b></p>".
                             "</div>". 
                         "</div>".
--- property-details.php
+++ property-details.php
@@ -316,24 +316,33 @@
                    {
                        echo "<div id=".$row["div_id"]." class="."col-lg-4"." data-position=".$row["name"].">".
                                 "<div class="."property-entry".">".
-                                    "<a href="."property-details.php?object=".$row["order_number"]." class="."property-thumbnail".">".
+                                    "<a href=".$row["has_details_path"]." class=".$row["a_active"]." >".
                                         "<img src=".$row["image_path"]." alt="."Image"." class="."img-fluid"." style="."width:100%; height:50%".">".
                                     "</a>".
-                                "<div class="."property-body".">".
-                                    "<h2 class="."property-title"."><a href="."property-details.php?object=".$row["order_number"].">".$row["name"]."</a></h2>".
-                                    "<br><span class="."property-location"."><span class="."icon-room"."></span>".$row["address_city"]."</span>".
-                                    "<br><br><strong class="."d-block"." style="."color:green; font-size:15px;".">".$row["description"]."</strong>".
-                                    "<br><ul class="."property-specs-wrap".">".
-                                         "<li>".
-                                             "<span class="."property-specs".">Broj proizvoda</span>".
-                                             "<span class="."property-specs-number".">".$row["number_of_products"]."</span>".
-                                         "</li>".
-                                    "</ul>".
-                                    "<p id=".$row["project_list_id"].">".$row["list_of_products"]."</p>".
-                                    "<p id=".$row["krizan_project_list_id"]."><b>".$row["list_of_krizan_products"]."</b></p>".
+                                    "<div class="."property-body".">".
+                                        "<div style="."display:inline-block;".">".
+                                            "<h2 class=".$row["title_active"].">
+                                                 <a href=".$row["has_details_path"].">".$row["name"]."</a>
+                                             </h2>".
+                                        "</div>".
+                                        "<div style="."display:inline-block;margin-left:10px;".">".
+                                            "<img src="."images/icons/Star.png"." style="."width:20px;display:".$row["star_visibility"].";"."></img>".
+                                        "</div>".
+                                        "<br><span class="."property-location"."><span class="."icon-room"."></span>".$row["address_city"]."</span>".
+                                        "<br><br><strong class="."d-block"." style="."color:green; font-size:15px;".">".$row["description"]."</strong>".
+                                        /*"<br><ul class="."property-specs-wrap".">".
+                                        "<li>".
+                                          "<span class="."property-specs".">Broj proizvoda</span>".
+                                           "<span class="."property-specs-number".">".$row["number_of_products"]."</span>".
+                                        "</li>".
+                                        "</ul>".*/
+                                       "<br/>".
+                                       "<p id=".$row["project_list_id"]."><b>Kategorija: </b>".$row["category_name"]."</p>".
+                                       "<a href=".$row["has_details_path"]." class=".$row["a_active"]." style="."display:".$row["display_details"].">Više detalja</a>".
+                                      "<p id=".$row["krizan_project_list_id"]." style="."display:none"."><b>".$row["list_of_krizan_products"]."</b></p>".
                                 "</div>". 
-                             "</div>".
-                           "</div>";
+                            "</div>".
+                        "</div>";
                     }
               }
               else {
